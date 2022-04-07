a3168588e95605b517d185eb1a2c2cc664532b78
leahdlrosa <100717677+leahdlrosa@users.noreply.github.com>
2022-04-07 14:52:59 -0500

    Created variables and initialized servo and button pins

--- MT_Project2/MT_Project2.ino
+++ MT_Project2/MT_Project2.ino
@@ -1,6 +1,22 @@
+
+/*
+   Hansel & Gretel
+   Team Name: Sisters Grimm
+   Team Members: Leah and Lizzie
+   Description: button and servo program
+*/
+
+//Servo Code Library
+#include<Servo.h>;
+
+//VARIABLES
+Servo myServo;
+const int buttonPin = 7;
+
 void setup() {
   // put your setup code here, to run once:
-
+  myServo.attach(9);
+  pinMode(buttonPin, INPUT);
 }
 
 void loop() {
